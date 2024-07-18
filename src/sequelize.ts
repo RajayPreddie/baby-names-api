23258ae03adfa6a2e9930b0ead930c8d84a247ae
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2024-07-18 01:23:28 -0400

    attempt to fix bug

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,11 +1,13 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import pg from 'pg';
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectModule: pg,
   logging: false
 });
 
